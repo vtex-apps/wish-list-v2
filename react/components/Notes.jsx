cc72a9b66bcbcd5ed602057af229572e5c94e4cc
Tharusha Silva <neeshsilva1@gmail.com>
2024-04-22 00:17:02 +0530

    Removed the color and note addtional icon.

--- react/components/Notes.jsx
+++ react/components/Notes.jsx
@@ -112,39 +112,6 @@ const Notes = ({
             strokeLinejoin="round"
           />
         </svg>
-
-        {!notes ? (
-          <svg
-            width="20px"
-            height="20px"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M7 12L12 12M12 12L17 12M12 12V7M12 12L12 17"
-              stroke="#000000"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        ) : (
-          <svg
-            width="20px"
-            height="20px"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M8.56078 20.2501L20.5608 8.25011L15.7501 3.43945L3.75012 15.4395V20.2501H8.56078ZM15.7501 5.56077L18.4395 8.25011L16.5001 10.1895L13.8108 7.50013L15.7501 5.56077ZM12.7501 8.56079L15.4395 11.2501L7.93946 18.7501H5.25012L5.25012 16.0608L12.7501 8.56079Z"
-              fill="#080341"
-            />
-          </svg>
-        )}
       </div>
 
       <Modal
