11fa393592cfd8a560a137cf5dc7493d8d12fe1f
Emanuel Ramos <emanuelramos@MacBook-Air-de-Emanuel-3.local>
2024-09-20 13:35:01 -0300

    feat: add regex to verify wishlist name has special characters

--- node/resolvers/createWishlist.ts
+++ node/resolvers/createWishlist.ts
@@ -1,6 +1,11 @@
 import type { WishlistInput } from '../typings/wishlist'
 import { auth } from '../middleware/auth'
 
+function hasSpecialCharacters(str: string) {
+  const specialCharactersRegex = /[!@$%^*(),.?":{}|<>]/g;
+  return specialCharactersRegex.test(str);
+}
+
 export const createWishlist = async (
   _: unknown,
   args: WishlistInput,
@@ -13,6 +18,10 @@ export const createWishlist = async (
   const { wishlistType } = args?.wishlist || {}
 
   const { email } = await auth(ctx)
+
+  if(hasSpecialCharacters(wishlistType)) {
+    throw new Error(`An error occurred. No special characters are allowed in the name.`)
+  }
 
   const foundWishlist = await md.searchForExistingWishList(
     `wishlistType = "${encodeURIComponent(wishlistType)}" AND email = ${email}`
